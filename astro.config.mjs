79f13813a3005636cb6c4195172cb2da1bf34b81
LeopoldHock <leopold.hock@deutschebahn.com>
2022-08-23 16:05:31 +0200

    try another config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://git.tech.rz.db.de', // my company's gitlab domain
-  base: '/LeopoldHock/astro-on-gitlab', // path deriving from my user and repository name
+  sitemap: true,
+  site: 'https://git.tech.rz.db.de/LeopoldHock/astro-on-gitlab', // my company's gitlab domain
   outDir: './public', // this is where gitlab pages expects the output
   publicDir: './static', // needs to be changed because it conflicts with where gitlab pages expects the build
 });
